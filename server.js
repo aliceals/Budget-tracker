bf2d9e30a617e6b4c552bb4d65d208e2de9e39a3
Alice Alsford <alice.alsford@gmail.com>
2019-07-14 15:05:18 +1200

    set up post request

--- server.js
+++ server.js
@@ -4,4 +4,14 @@ const port = 3000;
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
+app.post("/calculatetax", function(req, res) {
+  res.send("POST request to the homepage");
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+//post
+//Post (url, request body, response body)
+//POST /gettax
+//req body = {salary:100,000, taxCode: M}
+//response body = {weeklyPaye: $1000}
