d257a274a08bef0629bb93f43b08a3413512fa68
Alice Alsford <alice.alsford@gmail.com>
2019-07-14 15:19:39 +1200

    console logged the request

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
+
+app.use(express.json());
 const port = 3000;
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
 app.post("/calculatetax", function(req, res) {
   res.send({ weeklyPaye: 1000 });
+  console.log(req.body);
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
